4110c5b22d9e121964fb0a6e805869e5c0fed75e
LAiM0 <leo.aimonetto@insa-lyon.fr>
2024-03-21 17:14:19 +0100

    add chakra UI

--- node-app/dist/user/user.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { UserService } from './user.service';
-import { User } from './user.schema';
-export declare class UserController {
-    private readonly userService;
-    constructor(userService: UserService);
-    create(userName: string): Promise<User>;
-    findAll(): Promise<User[]>;
-    welcome(): Promise<string>;
-}
--- react-app/pages/index.js
+++ react-app/pages/index.js
@@ -1,6 +1,8 @@
 /* eslint-disable */
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { ChakraProvider, Button } from '@chakra-ui/react'; // Importez ChakraProvider et Button
+
 
 const Home = () => {
   const [welcomeMessage, setWelcomeMessage] = useState('...loading');
@@ -20,10 +22,13 @@ const Home = () => {
   }, []);
 
   return (
-    <div className='App'>
-      <h1>"Hello server!" says the client (NEXT)</h1>
-      <h1>"{welcomeMessage}" says the server (NEXT)</h1>
-    </div>
+    <ChakraProvider> {/* Utilisez ChakraProvider pour englober votre app */}
+      <div className='App'>
+        <h1>"Hello server!" says the client (NEXT)</h1>
+        <h1>"{welcomeMessage}" says the server (NEXT)</h1>
+        <Button colorScheme="blue">Test Button Chakra UI</Button> {/* Ajoutez un Button pour tester */}
+      </div>
+    </ChakraProvider>
   );
 };
 
