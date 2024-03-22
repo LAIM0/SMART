9cfa8d82223298ebbae6907ea67b7a70a11563b2
LAiM0 <leo.aimonetto@insa-lyon.fr>
2024-03-22 14:06:53 +0100

    edit index.js

--- react-app/pages/index.js
+++ react-app/pages/index.js
@@ -1,20 +1,19 @@
 /* eslint-disable */
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { ChakraProvider, Button } from '@chakra-ui/react'; // Importez ChakraProvider et Button
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { ChakraProvider, Button } from "@chakra-ui/react"; // Importez ChakraProvider et Button
 
 const Home = () => {
-  const [welcomeMessage, setWelcomeMessage] = useState('...loading');
+  const [welcomeMessage, setWelcomeMessage] = useState("...loading");
 
   useEffect(() => {
     const fetchWelcomeMessage = async () => {
       try {
-        const res = await axios.get('http://localhost:3001/users/welcome');
+        const res = await axios.get("http://localhost:3001/users/welcome");
         setWelcomeMessage(res.data);
       } catch (error) {
         console.log(error);
-        setWelcomeMessage('Failed to load message from server');
+        setWelcomeMessage("Failed to load message from server");
       }
     };
 
@@ -22,11 +21,14 @@ const Home = () => {
   }, []);
 
   return (
-    <ChakraProvider> {/* Utilisez ChakraProvider pour englober votre app */}
-      <div className='App'>
+    <ChakraProvider>
+      {" "}
+      {/* Utilisez ChakraProvider pour englober votre app */}
+      <div className="App">
         <h1>"Hello server!" says the client (NEXT)</h1>
         <h1>"{welcomeMessage}" says the server (NEXT)</h1>
-        <Button colorScheme="blue">Test Button Chakra UI</Button> {/* Ajoutez un Button pour tester */}
+        <Button colorScheme="blue">Test Button Chakra UI</Button>{" "}
+        {/* Ajoutez un Button pour tester */}
       </div>
     </ChakraProvider>
   );
