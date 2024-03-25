f8f52ef637f048808fefe0d715582d12671bf985
yboady <yanice.boady@insa-lyon.fr>
2024-03-25 09:50:46 +0100

    !animations

--- react-app/components/Challenges.jsx
+++ react-app/components/Challenges.jsx
@@ -19,7 +19,9 @@ function Challenges() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("http://localhost:3001/challenges/all");
+        const response = await axios.get(
+          "http://localhost:3001/challenges/all"
+        );
         setChallenge(response.data);
       } catch (error) {
         console.error("Erreur lors de la récupération des données:", error);
@@ -33,14 +35,22 @@ function Challenges() {
   const [currentCategory, setCurrentCategory] = useState(Tous);
 
   return (
-    <Flex flexDirection="column" gap={4} bg="#F8F8F8" p={8}>
+    <Flex flexDirection="column" gap={4} bg="#F8F8F8" p={8} overflow="hidden">
       <Heading>Défis</Heading>
-      <Flex p={3} gap={3} bg="white" borderRadius={8} boxShadow="sm">
+      <Flex
+        p={3}
+        gap={3}
+        bg="white"
+        borderRadius={8}
+        boxShadow="sm"
+        overflowX="scroll"
+      >
         <Box
           bg={"Tous" === currentCategory.categoryName ? "#166879" : "white"}
           onClick={() => setCurrentCategory(Tous)}
           _hover={{
-            bg: "Tous" === currentCategory.categoryName ? "166879" : "lightgray",
+            bg:
+              "Tous" === currentCategory.categoryName ? "166879" : "lightgray",
             cursor: "pointer"
           }}
           borderRadius={4}
@@ -49,47 +59,92 @@ function Challenges() {
           py={2}
           textAlign="center"
           fontSize={12}
-          fontWeight={"Tous" === currentCategory.categoryName ? "bold" : "normal"}
+          fontWeight={
+            "Tous" === currentCategory.categoryName ? "bold" : "normal"
+          }
         >
           <Heading size="sm">Tous</Heading>
         </Box>
         {categories.map((category) => (
           <Box
-            bg={category.categoryName === currentCategory.categoryName ? "#166879" : "white"}
+            bg={
+              category.categoryName === currentCategory.categoryName
+                ? "#166879"
+                : "white"
+            }
             key={category}
             onClick={() => setCurrentCategory(category)}
             _hover={{
-              bg: category.categoryName === currentCategory.categoryName ? "166879" : "lightgray",
+              bg:
+                category.categoryName === currentCategory.categoryName
+                  ? "166879"
+                  : "lightgray",
               cursor: "pointer"
             }}
             borderRadius={4}
-            color={category.categoryName === currentCategory.categoryName ? "white" : "#166879"}
+            color={
+              category.categoryName === currentCategory.categoryName
+                ? "white"
+                : "#166879"
+            }
             px={4}
             py={2}
             textAlign="center"
             fontSize={12}
-            fontWeight={category.categoryName === currentCategory.categoryName ? "bold" : "normal"}
+            fontWeight={
+              category.categoryName === currentCategory.categoryName
+                ? "bold"
+                : "normal"
+            }
           >
             <Heading size="sm">{category.categoryName}</Heading>
           </Box>
         ))}
       </Flex>
-
-      <Heading>À relever : {currentCategory.categoryName}</Heading>
+      <Heading>À relever</Heading>
       {challenges.map((challenge) => (
-        <Flex gap={4} wrap="wrap">
+        <Flex wrap="wrap">
           {(currentCategory.categoryName === "Tous" ||
             currentCategory._id === challenge.category) && (
-            <Card borderRadius={12} bg="white" key={challenge._id} p={4} gap={2} maxWidth="400px">
+            <Card
+              boxShadow="md"
+              borderRadius={12}
+              bg="white"
+              key={challenge._id}
+              p={4}
+              gap={2}
+              maxWidth="400px"
+              marginBottom={2}
+              transition="transform 0.3s ease"
+              _hover={{
+                transform: "translate(20px)",
+                cursor: "pointer"
+              }}
+            >
               <Heading size="md">{challenge.title}</Heading>
               <p>{challenge.description}</p>
-              <Flex gap={4}>
-                <Box width="auto" bg="#166879" color="white" p={2} borderRadius={8}>
+              <Flex gap={2}>
+                <Box
+                  width="auto"
+                  bg="#166879"
+                  color="white"
+                  p={2}
+                  borderRadius={8}
+                >
                   <Text fontWeight="bold">{challenge.points} pts</Text>
                 </Box>
-                <Box width="auto" bg="#4FD1C5" color="white" p={2} borderRadius={8}>
+                <Box
+                  width="auto"
+                  bg="#4FD1C5"
+                  color="white"
+                  p={2}
+                  borderRadius={8}
+                >
                   <Text fontWeight="bold">
-                    {Math.floor((challenge.endDate - new Date()) / (1000 * 60 * 60 * 24))} jours
+                    {Math.floor(
+                      (challenge.endDate - new Date()) / (1000 * 60 * 60 * 24)
+                    )}{" "}
+                    jours
                   </Text>
                 </Box>
               </Flex>
--- react-app/styles/theme.tsx
+++ react-app/styles/theme.tsx
@@ -4,25 +4,35 @@ const theme = extendTheme({
   colors: {
     primary: {
       300: "#166879",
-      100: "#D3E0E2",
+      100: "#D3E0E2"
       // Ajoutez d'autres nuances de couleur selon vos besoins
     },
     seondary: {
-      300: "#54C8C3",
+      300: "#54C8C3"
       // Ajoutez d'autres nuances de couleur selon vos besoins
       // Ajoutez d'autres palettes de couleurs selon vos besoins
-    },
+    }
   },
   radii: {
     sm: "4px", // Petit rayon
     md: "8px", // Rayon moyen (par défaut)
     lg: "12px", // Grand rayon
-    xl: "16px", // Très grand rayon
+    xl: "16px" // Très grand rayon
   },
   fonts: {
     body: "Poppins, sans-serif",
-    heading: "Poppins, sans-serif",
+    heading: "Poppins, sans-serif"
   },
+  styles: {
+    global: {
+      "*::-webkit-scrollbar": {
+        display: "none" // Masque la scrollbar pour les navigateurs basés sur WebKit (comme Chrome et Safari)
+      },
+      "*": {
+        scrollbarWidth: "none" // Masque la scrollbar pour les autres navigateurs
+      }
+    }
+  }
   // Ajoutez d'autres modifications de thème selon vos besoins
 });
 
