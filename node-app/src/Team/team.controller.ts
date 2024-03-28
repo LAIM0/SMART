1a8bb6e05b2924fdda1881bd173f0f3e34da3065
MatheoInsa <matheo05.ml@gmail.com>
2024-03-28 10:50:17 +0100

    Feat Signup Team par default

--- node-app/src/Team/team.controller.ts
+++ node-app/src/Team/team.controller.ts
@@ -13,11 +13,23 @@ export class TeamController {
     return this.teamService.findAll();
   }
 
-  
   @Post()
   async create(@Body() createTeamDto: CreateTeamDto): Promise<TeamDto> {
     return this.teamService.create(createTeamDto);
   } 
+
+  @Post('default')
+  async createDefaultTeam() {
+    try {
+      const defaultTeamDto: CreateTeamDto = {
+        name: 'Equipe par défaut',
+        // Autres détails de l'équipe par défaut
+      };
+      return this.teamService.create(defaultTeamDto);
+    } catch (error) {
+      throw new Error('Unable to create default team: ' + error.message);
+    }
+  }
 
   
 }
