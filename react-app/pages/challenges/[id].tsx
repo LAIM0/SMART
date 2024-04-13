dfed841f43d05580ea1d1d16746b1e44dba0124b
yboady <yanice.boady@insa-lyon.fr>
2024-04-13 23:27:36 +0200

    refactor: theme et front des challenges

--- react-app/pages/challenges/[id].tsx
+++ react-app/pages/challenges/[id].tsx
@@ -5,7 +5,6 @@ import { useRouter } from 'next/router';
 import {
   Box,
   Flex,
-  Heading,
   Text,
   IconButton,
   Button,
@@ -81,16 +80,31 @@ function Challenge() {
       if (fetchCompletedChallenge.length == 0) {
         setIsCompleted(false);
       } else {
-        console.log('on est bons');
         setIsCompleted(true);
-        console.log(isCompleted);
       }
     }
   }
 
+  const [windowWidth, setWindowWidth] = useState(0);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    handleResize(); // Appel initial pour définir la largeur de la fenêtre
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   useEffect(() => {
     checkCompletedChallenge();
   }, [user, router.query.id]);
+
+  const date = (currentChallenge && currentChallenge.endDate) || new Date();
 
   return (
     <ChakraProvider theme={theme}>
@@ -101,7 +115,6 @@ function Challenge() {
           mt="-64px"
           position="absolute"
           height="100vh"
-          width="100%"
           bg="tertiary"
           flexDirection="column"
         >
@@ -109,7 +122,8 @@ function Challenge() {
             flexDirection="column"
             h="25vh"
             bg="darkgray"
-            px="54px"
+            pr="54px"
+            pl="72px"
             justifyContent="flex-end"
             gap="8px"
             py="16px"
@@ -123,47 +137,76 @@ function Challenge() {
               width="fit-content"
               onClick={() => router.push('/challenges')}
             />
-            <Heading size="lg">{currentChallenge?.title}</Heading>
+            <Text as="h1" mb="0px">
+              {currentChallenge?.title}
+            </Text>
             <Flex gap={2}>
               <Box
                 width="auto"
-                bg="#166879"
+                bg="primary.300"
                 color="white"
-                p={2}
+                px="16px"
+                py="8px"
                 borderRadius={8}
               >
-                <Text fontWeight="bold">{currentChallenge?.points} pts</Text>
+                <Text as="h4" color="white">
+                  {currentChallenge?.points} pts
+                </Text>
               </Box>
               <Box
                 width="auto"
                 bg="#4FD1C5"
                 color="white"
-                p={2}
+                px="16px"
+                py="8px"
                 borderRadius={8}
               >
-                <Text fontWeight="bold">3 jours</Text>
+                <Text as="h4" color="white">
+                  {' '}
+                  {isCompleted
+                    ? 'Déjà réalisé'
+                    : ` Il vous reste ${dateGap(date)} jours`}
+                </Text>
               </Box>
             </Flex>
           </Flex>
           <Flex
             gap={4}
             flexDirection="column"
-            px="54px"
+            pr="54px"
+            pl="72px"
             py="16px"
             boxShadow="0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)"
+            position="relative"
           >
-            <Heading size="md">Description</Heading>
+            <Text as="h2">Description</Text>
             <p>{currentChallenge?.description}</p>
-            <Heading size="md">Ressources</Heading>
+            <Text as="h2">Ressources</Text>
             <Text>{currentChallenge?.pedagogicalExplanation}</Text>
 
             <Button
-              bg={isCompleted ? '#FFFFFF' : '#54C8C3'}
+              bg={isCompleted ? '#FFFFFF' : 'secondary.300'}
+              _hover={{
+                boxShadow: isCompleted
+                  ? '0 0px 0px rgba(0,0,0,0)'
+                  : '0 8px 24px rgba(51, 193, 177, 1)',
+              }}
+              position={windowWidth < 500 ? 'fixed' : 'absolute'}
+              right={windowWidth < 500 ? '50%' : '54px'}
+              transform={windowWidth < 500 ? 'translate(50%,0)' : 'auto'}
+              top={windowWidth < 500 ? 'auto' : '-30px'}
+              bottom={windowWidth < 500 ? '160px' : '#FFFFFF'}
+              fontSize="h3"
+              fontWeight="semiBold"
+              px="32px"
+              py="24px"
+              borderRadius="16px"
+              transition=" box-shadow 0.3s ease"
               width="fit-content"
               border="2px solid"
-              borderColor="#54C8C3"
-              color={isCompleted ? '#54C8C3' : '#FFFFFF'}
-              boxShadow={isCompleted ? 'null' : 'md'}
+              borderColor="secondary.300"
+              color={isCompleted ? 'secondary.300' : '#FFFFFF'}
+              boxShadow={isCompleted ? 'null' : 'button'}
               onClick={() => {
                 if (isCompleted) {
                   deleteCompleted();
