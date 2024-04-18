675675cb54c230ed43e81e38ce3bcdee92181111
yboady <yanice.boady@insa-lyon.fr>
2024-04-18 18:34:32 +0200

    feat:IA

---
+++ react-app/pages/test-IA.jsx
@@ -0,0 +1,12 @@
+function TextGenerationApp() {
+  return (
+    <iframe
+      src="https://yboady-ia-coexya.hf.space"
+      frameborder="0"
+      width="850"
+      height="450"
+    ></iframe>
+  );
+}
+
+export default TextGenerationApp;
