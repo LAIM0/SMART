d5fb54ed127006de961a6f06775e33612b9bb545
yboady <yanice.boady@insa-lyon.fr>
2024-04-19 14:08:20 +0200

    improve IA

--- react-app/pages/test-IA.jsx
+++ react-app/pages/test-IA.jsx
@@ -1,11 +1,24 @@
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import Layout from '../components/Layout/Layout';
+import theme from '../styles/theme';
+import { Flex, Text, Box } from '@chakra-ui/react';
+
 function TextGenerationApp() {
   return (
-    <iframe
-      src="https://yboady-ia-coexya.hf.space"
-      frameborder="0"
-      width="850"
-      height="450"
-    ></iframe>
+    <ChakraProvider theme={theme}>
+      <Layout>
+        <Flex flexDirection="column" px="32px" py="24px">
+          <Text as="h1">EcoexIA</Text>
+          <iframe
+            src="https://yboady-ia-coexya.hf.space"
+            frameborder="0"
+            width="850"
+            height="450"
+          ></iframe>
+        </Flex>
+      </Layout>
+    </ChakraProvider>
   );
 }
 
