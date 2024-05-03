2db8632441b2fe94116844b060a71271a16173bc
Maxime Brun <mbrun2001@gmail.com>
2024-05-03 12:12:58 +0200

    feat: backend create entity image

--- react-app/api/apiUtils/apiConfig.ts
+++ react-app/api/apiUtils/apiConfig.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-export const API_BASE_URL = 'http://localhost:3001';
+export const apiBaseUrl = 'http://localhost:3001';
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: apiBaseUrl,
   headers: { 'Content-Type': 'application/json' },
   // Vous pouvez ajouter d'autres en-têtes par défaut ici si nécessaire
 });
